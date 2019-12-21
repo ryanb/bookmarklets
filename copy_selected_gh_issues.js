002c984877d91a8ea0dc96bd9583579557eea708
Ryan Bates <ryan@railscasts.com>
2019-12-20 17:33:17 -0800

    Initial bookmarklets

---
+++ copy_selected_gh_issues.js
@@ -0,0 +1,23 @@
+javascript:(function() {
+  function copyToClipboard(str) {
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  };
+  let notes = "";
+  let elements = document.querySelectorAll(".js-issue-row.selected .js-navigation-open");
+  elements = [...elements].reverse();
+  for (const element of elements) {
+    let number = element.id.split("_")[1];
+    notes += `* [${element.textContent} (#${number})](${element.href})\n`;
+  }
+  copyToClipboard(notes);
+  let checkboxes = document.querySelectorAll(".js-issue-row.selected input[type='checkbox']");
+  for (const checkbox of checkboxes) {
+    checkbox.click();
+  }
+  console.log("Copied selection to clipboard!");
+})();
---
+++ copy_selected_pt_stories.js
@@ -0,0 +1,23 @@
+javascript:(function() {
+  function copyToClipboard(str) {
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  };
+  let notes = "";
+  $("a.selector.selected:visible").each(function(index, element) {
+    let $div = $(element).closest("div.story");
+    let story_id = $div.data("id");
+    let story_text = $div.find("span.story_name span.tracker_markup").text();
+    let line = `* [${story_text}](https://www.pivotaltracker.com/story/show/${story_id})\n`;
+    if (!notes.includes(line)) {
+      notes += line;
+    }
+  });
+  copyToClipboard(notes);
+  $("[data-aid='BulkDeselect']").trigger("click");
+  console.log("Copied stories to clipboard!");
+})();
