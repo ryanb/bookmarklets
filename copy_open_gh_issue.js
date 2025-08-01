cb2521ab7b6aef53b08365c6464c8a8d97953236
Ryan Bates <ryan@railscasts.com>
2025-08-01 11:31:11 -0700

    Add lines added and removed when copying open GH issue

--- copy_open_gh_issue.js
+++ copy_open_gh_issue.js
@@ -10,6 +10,8 @@ javascript:(function() {
   let name = document.querySelector(".js-issue-title").textContent.trim();
   let number = document.querySelector(".gh-header-number").textContent.trim();
   let url = window.location.href;
-  copyToClipboard(`[${name} (${number})](${url})`);
+  let addedLines = document.querySelector("#diffstat .color-fg-success").textContent.trim();
+  let removedLines = document.querySelector("#diffstat .color-fg-danger").textContent.trim();
+  copyToClipboard(`[${name} (${number})](${url}) [${addedLines}, ${removedLines}]`);
   console.log("Copied current GitHub issue to clipboard!");
 })();
