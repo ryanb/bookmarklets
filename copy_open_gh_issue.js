39b62edf9600fd08a7cc6bcc31c82827dfe2fa63
Ryan Bates <ryan@railscasts.com>
2026-02-06 13:36:55 -0800

    Fix Open GH Issue when there are no added or removed lines

--- copy_open_gh_issue.js
+++ copy_open_gh_issue.js
@@ -9,8 +9,8 @@ javascript:(function() {
   };
   const [name, number] = [...document.querySelectorAll("[data-component='TitleArea'] h1 span")].map((element) => element.textContent);
   const url = window.location.href;
-  const addedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-success").textContent.trim();
-  const removedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-danger").textContent.trim();
+  const addedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-success")?.textContent.trim() || "+0";
+  const removedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-danger")?.textContent.trim() || "-0";
   copyToClipboard(`[${name} (${number})](${url}) [${addedLines}, ${removedLines}]`);
   console.log("Copied current GitHub issue to clipboard!");
 })();
