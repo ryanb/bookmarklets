44e585ecb1d2a133ab89d5dad3869b64505f7cca
Ryan Bates <ryan@railscasts.com>
2026-02-09 17:11:44 -0800

    Add fallbacks to Copy Open GH Issue so it works for both old and new DOM structure

--- copy_open_gh_issue.js
+++ copy_open_gh_issue.js
@@ -7,10 +7,20 @@ javascript:(function() {
     document.execCommand("copy");
     document.body.removeChild(el);
   };
-  const [name, number] = [...document.querySelectorAll("[data-component='TitleArea'] h1 span")].map((element) => element.textContent);
+  let name = document.querySelector("h1.gh-header-title bdi")?.textContent.trim();
+  let number = document.querySelector("h1.gh-header-title span.gh-header-number, h1.gh-header-title span.color-fg-muted")?.textContent.trim();
+  if (!name || !number) {
+    const spans = [...document.querySelectorAll("[data-component='TitleArea'] h1 span")].map((element) => element.textContent);
+    name = spans[0];
+    number = spans[1];
+  }
   const url = window.location.href;
-  const addedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-success")?.textContent.trim() || "+0";
-  const removedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-danger")?.textContent.trim() || "-0";
+  let addedLines = document.querySelector("#diffstat span.color-fg-success")?.textContent.trim();
+  let removedLines = document.querySelector("#diffstat span.color-fg-danger")?.textContent.trim();
+  if (!addedLines || !removedLines) {
+    addedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-success")?.textContent.trim() || "+0";
+    removedLines = document.querySelector("[data-component='PH_Navigation'] span.fgColor-danger")?.textContent.trim() || "-0";
+  }
   copyToClipboard(`[${name} (${number})](${url}) [${addedLines}, ${removedLines}]`);
   console.log("Copied current GitHub issue to clipboard!");
 })();
